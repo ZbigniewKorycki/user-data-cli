f7934e139b9d63210edfde885809e5370c50c15d
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-11-28 11:16:22 +0100

    add logging setup and config

---
+++ logging_setup.py
@@ -0,0 +1,12 @@
+import logging
+import logging.config
+import yaml
+
+
+def setup_logging(logger_name):
+    with open("logging_config.yaml", "r") as f:
+        config = yaml.safe_load(f.read())
+
+    logging.config.dictConfig(config)
+    logger = logging.getLogger(logger_name)
+    return logger
