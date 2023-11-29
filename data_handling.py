b487631731577c57a169991d22a0c452f060c4f9
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-11-29 20:45:34 +0100

    correct static typing

--- data_handling.py
+++ data_handling.py
@@ -1,12 +1,9 @@
 import re
 import xmltodict
 import xml.etree.ElementTree as ET
-from typing import List
+from typing import List, Optional, Union
 import csv
 import json
-from scripts import logging_setup
-
-logger = logging_setup.setup_logging(__name__)
 
 
 class TelephoneHandler:
@@ -17,7 +14,7 @@ class TelephoneHandler:
         return re.sub(cls.FORMATTING_PATTERN, "", number)
 
     @staticmethod
-    def is_phone_present(number) -> bool:
+    def is_phone_present(number: Union[str, dict]) -> bool:
         return False if (number == "") or (number is None) or (number == []) else True
 
 
@@ -25,35 +22,37 @@ class EmailHandler:
     VALIDATION_PATTERN = r"(^[^@]+@[^@\.]+\.[a-z\d]{1,4}$)"
 
     @classmethod
-    def is_email_valid(cls, email) -> bool:
+    def is_email_valid(cls, email: Union[str, dict]) -> bool:
         try:
             validation = re.match(cls.VALIDATION_PATTERN, email, re.IGNORECASE)
         except TypeError:
-            logger.debug("TypeError in email validation")
             return False
         else:
             return True if validation else False
 
 
 class DataConverter:
-
     @staticmethod
     def extract_file_extension_from_path(path_to_file: str) -> str:
         return path_to_file.rsplit(".", 1)[1]
 
     @staticmethod
-    def if_user_has_children(user: dict):
-        return False if (user["children"] == "") or (user["children"] is None) or (user["children"] == []) else True
+    def if_user_has_children(user: dict) -> bool:
+        return (
+            False
+            if (user["children"] == "")
+               or (user["children"] is None)
+               or (user["children"] == [])
+            else True
+        )
 
 
 class DataConverterXML(DataConverter):
-
     def __init__(self):
         super().__init__()
 
     @staticmethod
-    def get_children_info_from_xml(user: dict):
-        logger.info(f"{user}")
+    def get_children_info_from_xml(user: dict) -> Optional[Union[dict, list]]:
         if not DataConverterXML.if_user_has_children(user):
             return None
         try:
@@ -70,22 +69,27 @@ class DataConverterXML(DataConverter):
         return xmltodict.parse(ET.tostring(root))
 
     @staticmethod
-    def format_user_from_xml(user: dict) -> dict:
-        if not TelephoneHandler.is_phone_present(user.get("telephone_number")) or not EmailHandler.is_email_valid(
-                user.get("email")):
+    def format_user_from_xml(user: dict) -> Optional[dict]:
+        if not TelephoneHandler.is_phone_present(
+                user.get("telephone_number")
+        ) or not EmailHandler.is_email_valid(user.get("email")):
             return None
-        user["telephone_number"] = TelephoneHandler.format_number(user["telephone_number"])
+        user["telephone_number"] = TelephoneHandler.format_number(
+            user["telephone_number"]
+        )
         user["children"] = DataConverterXML.get_children_info_from_xml(user)
         return user
 
     @staticmethod
     def filter_valid_users_from_xml(data: List[dict]) -> List[dict]:
-        return [DataConverterXML.format_user_from_xml(user) for user in data if
-                DataConverterXML.format_user_from_xml(user)]
+        return [
+            DataConverterXML.format_user_from_xml(user)
+            for user in data
+            if DataConverterXML.format_user_from_xml(user)
+        ]
 
 
 class DataConverterCSV(DataConverter):
-
     def __init__(self):
         super().__init__(self)
 
@@ -95,37 +99,41 @@ class DataConverterCSV(DataConverter):
             return csv.DictReader(csvfile, delimiter=";")
 
     @staticmethod
-    def get_children_info_from_csv(user: dict):
+    def get_children_info_from_csv(user: dict) -> Optional[Union[dict, list]]:
         if not DataConverterCSV.if_user_has_children(user):
             return None
-        children = [
-            child.strip() for child in user["children"].split(",")
-        ]
+        children = [child.strip() for child in user["children"].split(",")]
         children_info = [
             {
                 "name": child.split("(")[0].strip(),
                 "age": int(child.split("(")[1].replace(")", "").strip()),
             }
-            for child in children]
+            for child in children
+        ]
         return children_info
 
     @staticmethod
-    def format_user_from_csv(user: dict) -> dict:
-        if not TelephoneHandler.is_phone_present(user.get("telephone_number")) or EmailHandler.is_email_valid(
-                user.get("email")):
+    def format_user_from_csv(user: dict) -> Optional[dict]:
+        if not TelephoneHandler.is_phone_present(
+                user.get("telephone_number")
+        ) or EmailHandler.is_email_valid(user.get("email")):
             return None
-        user["telephone_number"] = TelephoneHandler.format_number(user["telephone_number"])
+        user["telephone_number"] = TelephoneHandler.format_number(
+            user["telephone_number"]
+        )
         user["children"] = DataConverterCSV.get_children_info_from_csv(user)
         return user
 
     @staticmethod
     def filter_valid_users_from_csv(data: List[dict]) -> List[dict]:
-        return [DataConverterCSV.format_user_from_csv(user) for user in data if
-                DataConverterCSV.format_user_from_csv(user)]
+        return [
+            DataConverterCSV.format_user_from_csv(user)
+            for user in data
+            if DataConverterCSV.format_user_from_csv(user)
+        ]
 
 
 class DataConverterJSON(DataConverter):
-
     def __init__(self):
         super().__init__(self)
 
@@ -136,7 +144,7 @@ class DataConverterJSON(DataConverter):
         return data
 
     @staticmethod
-    def get_children_info_from_json(user: dict):
+    def get_children_info_from_json(user: dict) -> Optional[Union[dict, list]]:
         if not DataConverterJSON.if_user_has_children(user):
             return None
         try:
@@ -147,15 +155,21 @@ class DataConverterJSON(DataConverter):
             return children_info
 
     @staticmethod
-    def format_user_from_json(user: dict) -> dict:
-        if not TelephoneHandler.is_phone_present(user.get("telephone_number")) or not EmailHandler.is_email_valid(
-                user.get("email")):
+    def format_user_from_json(user: dict) -> Optional[dict]:
+        if not TelephoneHandler.is_phone_present(
+                user.get("telephone_number")
+        ) or not EmailHandler.is_email_valid(user.get("email")):
             return None
-        user["telephone_number"] = TelephoneHandler.format_number(user["telephone_number"])
+        user["telephone_number"] = TelephoneHandler.format_number(
+            user["telephone_number"]
+        )
         user["children"] = DataConverterJSON.get_children_info_from_json(user)
         return user
 
     @staticmethod
     def filter_valid_users_from_json(data: List[dict]) -> List[dict]:
-        return [DataConverterJSON.format_user_from_json(user) for user in data if
-                DataConverterJSON.format_user_from_json(user)]
+        return [
+            DataConverterJSON.format_user_from_json(user)
+            for user in data
+            if DataConverterJSON.format_user_from_json(user)
+        ]
