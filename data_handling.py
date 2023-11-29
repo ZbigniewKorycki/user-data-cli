9e86f46fd3f50dcfc93a07f5f3f883baf2d12c33
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-11-29 14:42:58 +0100

    add func json_to_dict_list_with_email_and_phone_verification, rename classes TelephoneFormatter, EmailValidator

--- data_handling.py
+++ data_handling.py
@@ -1,30 +1,42 @@
 import re
 import xmltodict
 import xml.etree.ElementTree as ET
-from typing import Callable, List
+from typing import List
 import csv
+import json
 
 
-class TelephoneFormatter:
+class TelephoneHandler:
+    FORMATTING_PATTERN = r"\s|\+48|\(48\)|^00"
+
+    @classmethod
+    def format_number(cls, number: str) -> str:
+        return re.sub(cls.FORMATTING_PATTERN, "", number)
+
     @staticmethod
-    def format_number(number: str) -> str:
-        pattern = r"\s|\+48|\(48\)|^00"
-        return re.sub(pattern, "", number)
+    def is_present(number: str) -> bool:
+        return False if (number == "") or (number is None) or (number == []) else True
 
 
-class EmailValidator:
+class EmailHandler:
+    VALIDATION_PATTERN = r"(^[^@]+@[^@\.]+\.[a-z\d]{1,4}$)"
+
+    @classmethod
+    def is_valid(cls, email: str) -> bool:
+        return True if re.match(cls.VALIDATION_PATTERN, email, re.IGNORECASE) else False
+
+
+class FileHandler:
+
     @staticmethod
-    def is_valid(email: str) -> bool:
-        validation_pattern = r"(^[^@]+@[^@]+\.[a-z\d]{1,4}$)"
-        return True if re.match(validation_pattern, email, re.IGNORECASE) else False
+    def extract_file_extension_from_path(path_to_file: str) -> str:
+        return path_to_file.rsplit(".", 1)[1]
 
 
 class DataConverter:
     @staticmethod
     def xml_to_dict_list_with_email_and_phone_verification(
             path_to_xml: str,
-            email_validator: Callable[[str], bool],
-            telephone_formatter: Callable[[str], str],
     ) -> List[dict]:
         tree = ET.parse(path_to_xml)
         root = tree.getroot()
@@ -33,41 +45,53 @@ class DataConverter:
         formatted_users_data = []
         for user in data_dict.get("users", {}).get("user", []):
             if (user.get("telephone_number") is None) or (
-                    not email_validator(user.get("email", ""))
+                    not EmailHandler.is_valid(user["email"])
             ):
                 continue
+            user["telephone_number"] = EmailHandler.format_number(
+                user["telephone_number"]
+            )
+            if user["children"]:
+                user["children"] = user["children"]["child"]
             else:
-                user["telephone_number"] = telephone_formatter(
-                    user.get("telephone_number", "")
-                )
-                formatted_users_data.append(user)
+                user["children"] = None
+            formatted_users_data.append(user)
         return formatted_users_data
 
     @staticmethod
-    def csv_to_dict_list_with_email_and_phone_verification(path_to_csv: str) -> List[dict]:
+    def csv_to_dict_list_with_email_and_phone_verification(
+            path_to_csv: str,
+    ) -> List[dict]:
         users_data = []
-        with open(path_to_csv, newline='') as csvfile:
-            reader = csv.DictReader(csvfile, delimiter=';')
+        with open(path_to_csv, newline="") as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=";")
             for row in reader:
                 children = []
-                if row.get('children'):
-                    children_info = [child.strip() for child in row['children'].split(',')]
+                if row["children"]:
+                    children_info = [
+                        child.strip() for child in row["children"].split(",")
+                    ]
                     children = [
-                        {'name': info.split('(')[0].strip(), 'age': int(info.split('(')[1].replace(')', '').strip())}
-                        for
-                        info in children_info]
-                email = row.get('email', '')
-                telephone = row.get('telephone_number', '')
-                if EmailValidator.is_valid(email) and telephone != '':
-                    telephone = TelephoneFormatter.format_number(telephone)
+                        {
+                            "name": info.split("(")[0].strip(),
+                            "age": int(info.split("(")[1].replace(")", "").strip()),
+                        }
+                        for info in children_info
+                    ]
+                else:
+                    children = None
+                email = row.get("email", "")
+                telephone = row.get("telephone_number", "")
+                if EmailHandler.is_valid(email) and telephone != "":
+                    telephone = TelephoneHandler.format_number(telephone)
                     user = {
-                        'firstname': row.get('firstname', ''),
-                        'telephone_number': telephone,
-                        'email': email,
-                        'password': row.get('password', ''),
-                        'role': row.get('role', ''),
-                        'created_at': row.get('created_at', ''),
-                        'children': children
+                        "firstname": row.get("firstname", ""),
+                        "telephone_number": telephone,
+                        "email": email,
+                        "password": row.get("password", ""),
+                        "role": row.get("role", ""),
+                        "created_at": row.get("created_at", ""),
+                        "children": children,
                     }
                     users_data.append(user)
                 else:
@@ -75,5 +99,22 @@ class DataConverter:
             return users_data
 
     @staticmethod
-    def json_to_dict_list_with_email_and_phone_verification(path_to_json: str) -> List[dict]:
-        pass
+    def json_to_dict_list_with_email_and_phone_verification(
+            path_to_json: str,
+    ) -> List[dict]:
+        user_data = []
+        with open(path_to_json) as file:
+            data = json.load(file)
+            for user in data:
+                if user["telephone_number"] != "" and EmailHandler.is_valid(
+                        user["email"]
+                ):
+                    user["telephone_number"] = TelephoneHandler.format_number(
+                        user["telephone_number"]
+                    )
+                    if not user["children"]:
+                        user["children"] = None
+                    user_data.append(user)
+                else:
+                    continue
+        return user_data
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,4 +1,4 @@
-from data_handling import TelephoneFormatter, EmailValidator
+from data_handling import TelephoneHandler, EmailHandler
 
 
 def test_email_address_validation():
@@ -11,16 +11,18 @@ def test_email_address_validation():
     invalid_two_consecutive_at_sign = "invalid@@example.com"
     invalid_at_sign_in_extension = "invalid_email@example.@"
     valid_email_uppercase = "VALID@gmail.com"
+    invalid_email_two_dots = "INVALID@..com"
 
-    assert EmailValidator.is_valid(valid_email) is True
-    assert EmailValidator.is_valid(another_valid_email) is True
-    assert EmailValidator.is_valid(invalid_email_no_domain_and_extension) is False
-    assert EmailValidator.is_valid(invalid_email_with_missing_at_sign) is False
-    assert EmailValidator.is_valid(invalid_email_with_no_dot) is False
-    assert EmailValidator.is_valid(invalid_missing_domain) is False
-    assert EmailValidator.is_valid(invalid_two_consecutive_at_sign) is False
-    assert EmailValidator.is_valid(invalid_at_sign_in_extension) is False
-    assert EmailValidator.is_valid(valid_email_uppercase) is True
+    assert EmailHandler.is_valid(valid_email) is True
+    assert EmailHandler.is_valid(another_valid_email) is True
+    assert EmailHandler.is_valid(invalid_email_no_domain_and_extension) is False
+    assert EmailHandler.is_valid(invalid_email_with_missing_at_sign) is False
+    assert EmailHandler.is_valid(invalid_email_with_no_dot) is False
+    assert EmailHandler.is_valid(invalid_missing_domain) is False
+    assert EmailHandler.is_valid(invalid_two_consecutive_at_sign) is False
+    assert EmailHandler.is_valid(invalid_at_sign_in_extension) is False
+    assert EmailHandler.is_valid(valid_email_uppercase) is True
+    assert EmailHandler.is_valid(invalid_email_two_dots) is False
 
 
 def test_telephone_formatter():
@@ -31,9 +33,9 @@ def test_telephone_formatter():
     phone_leading_zeros_and_spaces = "00 123 123 123"
     phone_correct = "123123123"
 
-    assert TelephoneFormatter.format_number(phone_leading_zeros) == "847940862"
-    assert TelephoneFormatter.format_number(phone_area_code_and_plus) == "844840862"
-    assert TelephoneFormatter.format_number(phone_area_code_in_parenthesis) == "123123123"
-    assert TelephoneFormatter.format_number(phone_with_spaces) == "123123123"
-    assert TelephoneFormatter.format_number(phone_leading_zeros_and_spaces) == "123123123"
-    assert TelephoneFormatter.format_number(phone_correct) == "123123123"
+    assert TelephoneHandler.format_number(phone_leading_zeros) == "847940862"
+    assert TelephoneHandler.format_number(phone_area_code_and_plus) == "844840862"
+    assert TelephoneHandler.format_number(phone_area_code_in_parenthesis) == "123123123"
+    assert TelephoneHandler.format_number(phone_with_spaces) == "123123123"
+    assert TelephoneHandler.format_number(phone_leading_zeros_and_spaces) == "123123123"
+    assert TelephoneHandler.format_number(phone_correct) == "123123123"
