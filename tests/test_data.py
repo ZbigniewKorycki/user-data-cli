e82e8c7be74e9dadd71e44acc4b5316790debc93
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-11-30 18:10:30 +0100

    update tests after changing class

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,8 @@
-from data_handling import TelephoneHandler, EmailHandler
+from users_data_handler import UsersDataProcessor
 
 
 def test_email_address_validation():
+
     valid_email = "valid@example.com"
     another_valid_email = "another_valid@gmail.com"
     invalid_email_no_domain_and_extension = "invalid_email@"
@@ -14,17 +15,17 @@ def test_email_address_validation():
     invalid_email_two_dots = "INVALID@..com"
     invalid_email_none = None
 
-    assert EmailHandler.is_email_valid(valid_email) is True
-    assert EmailHandler.is_email_valid(another_valid_email) is True
-    assert EmailHandler.is_email_valid(invalid_email_no_domain_and_extension) is False
-    assert EmailHandler.is_email_valid(invalid_email_with_missing_at_sign) is False
-    assert EmailHandler.is_email_valid(invalid_email_with_no_dot) is False
-    assert EmailHandler.is_email_valid(invalid_missing_domain) is False
-    assert EmailHandler.is_email_valid(invalid_two_consecutive_at_sign) is False
-    assert EmailHandler.is_email_valid(invalid_at_sign_in_extension) is False
-    assert EmailHandler.is_email_valid(valid_email_uppercase) is True
-    assert EmailHandler.is_email_valid(invalid_email_two_dots) is False
-    assert EmailHandler.is_email_valid(invalid_email_none) is False
+    assert UsersDataProcessor.is_email_address_valid(valid_email) is True
+    assert UsersDataProcessor.is_email_address_valid(another_valid_email) is True
+    assert UsersDataProcessor.is_email_address_valid(invalid_email_no_domain_and_extension) is False
+    assert UsersDataProcessor.is_email_address_valid(invalid_email_with_missing_at_sign) is False
+    assert UsersDataProcessor.is_email_address_valid(invalid_email_with_no_dot) is False
+    assert UsersDataProcessor.is_email_address_valid(invalid_missing_domain) is False
+    assert UsersDataProcessor.is_email_address_valid(invalid_two_consecutive_at_sign) is False
+    assert UsersDataProcessor.is_email_address_valid(invalid_at_sign_in_extension) is False
+    assert UsersDataProcessor.is_email_address_valid(valid_email_uppercase) is True
+    assert UsersDataProcessor.is_email_address_valid(invalid_email_two_dots) is False
+    assert UsersDataProcessor.is_email_address_valid(invalid_email_none) is False
 
 
 def test_telephone_formatter():
@@ -35,21 +36,9 @@ def test_telephone_formatter():
     phone_leading_zeros_and_spaces = "00 123 123 123"
     phone_correct = "123123123"
 
-    assert TelephoneHandler.format_number(phone_leading_zeros) == "847940862"
-    assert TelephoneHandler.format_number(phone_area_code_and_plus) == "844840862"
-    assert TelephoneHandler.format_number(phone_area_code_in_parenthesis) == "123123123"
-    assert TelephoneHandler.format_number(phone_with_spaces) == "123123123"
-    assert TelephoneHandler.format_number(phone_leading_zeros_and_spaces) == "123123123"
-    assert TelephoneHandler.format_number(phone_correct) == "123123123"
-
-
-def test_is_phone_present():
-    valid_phone = "123333333"
-    invalid_phone_None = None
-    invalid_phone_empty_list = []
-    invalid_phone_empty_str = ""
-
-    assert TelephoneHandler.is_phone_present(valid_phone) is True
-    assert TelephoneHandler.is_phone_present(invalid_phone_None) is False
-    assert TelephoneHandler.is_phone_present(invalid_phone_empty_list) is False
-    assert TelephoneHandler.is_phone_present(invalid_phone_empty_str) is False
+    assert UsersDataProcessor.format_telephone_number(phone_leading_zeros) == "847940862"
+    assert UsersDataProcessor.format_telephone_number(phone_area_code_and_plus) == "844840862"
+    assert UsersDataProcessor.format_telephone_number(phone_area_code_in_parenthesis) == "123123123"
+    assert UsersDataProcessor.format_telephone_number(phone_with_spaces) == "123123123"
+    assert UsersDataProcessor.format_telephone_number(phone_leading_zeros_and_spaces) == "123123123"
+    assert UsersDataProcessor.format_telephone_number(phone_correct) == "123123123"
