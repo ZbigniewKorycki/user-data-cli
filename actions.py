580e84b9a21644828ec96a7a93006dbf7bb18681
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-12-02 15:31:59 +0100

    implement logic for print to CLI, add find_users_with_similar_children_by_age function

--- actions.py
+++ actions.py
@@ -6,7 +6,6 @@ logger = logging_setup.setup_logging(__name__)
 
 
 class Actions:
-
     def __init__(self, login, password):
         self.login = login
         self.password = password
@@ -15,12 +14,15 @@ class Actions:
         self.user_data = None
         self.authenticate_user()
 
-
     def authenticate_user(self):
         try:
             user = data[
-                ((data["email"] == self.login) | (data["telephone_number"] == self.login)) & (
-                        data["password"] == self.password)].to_dict(orient="records")[0]
+                (
+                        (data["email"] == self.login)
+                        | (data["telephone_number"] == self.login)
+                )
+                & (data["password"] == self.password)
+                ].to_dict(orient="records")[0]
         except IndexError:
             logger.info("Invalid login - Not authenticate user")
         else:
@@ -52,6 +54,7 @@ class Actions:
                 return func(self, *args, **kwargs)
             else:
                 logger.info("Invalid login - authentication required")
+
         return wrapper
 
     @authentication_required
@@ -65,21 +68,38 @@ class Actions:
             print(f"{children_info['name']}, {children_info['age']}")
 
     @authentication_required
-    def find_users_with_similar_children(self):
-        pass
+    def find_users_with_similar_children_by_age(self):
+        user_children_age = [int(child["age"]) for child in self.user_data["children"]]
+        users_with_children = data[data["children"].notna()]
+        users_with_similar_children_age = users_with_children[
+            users_with_children["children"].apply(
+                lambda x: (any(child["age"] in user_children_age for child in x if isinstance(child, dict))))]
+        users_similar = users_with_similar_children_age.to_dict(orient="records")
+        for user in users_similar:
+            children_sorted = sorted(user["children"], key=lambda x: x["name"])
+            print(f"{user['firstname']}, {user['telephone_number']}: {children_sorted}")
 
     @admin_required
-    def count_all_users(self) -> int:
-        return len(data)
+    def print_all_accounts(self):
+        print(len(data))
 
     @admin_required
-    def get_oldest_account(self) -> dict:
-        return data.sort_values(by="created_at").to_dict(orient="records")[0]
+    def print_oldest_account(self):
+        oldest_account = data.sort_values(by="created_at").to_dict(orient="records")[0]
+        if oldest_account is not None:
+            print(
+                f"name: {oldest_account['firstname']}\n"
+                f"email_address: {oldest_account['email']}\n"
+                f"created_at: {oldest_account['created_at']}"
+            )
 
     @admin_required
-    def get_users_children_grouped_by_age(self):
+    def group_children_by_age(self):
+        # implement data[data["children"].notna()]
         children_data = data["children"].to_list()
-        filtered_children_without_none = [child for child in children_data if child is not None]
+        filtered_children_without_none = [
+            child for child in children_data if child is not None
+        ]
         children = []
         for user_children in filtered_children_without_none:
             if isinstance(user_children, list):
@@ -88,7 +108,12 @@ class Actions:
             else:
                 children.append(int(user_children["age"]))
         sorted_data = sorted(children)
-        grouped_data = sorted([{"age": key, "count": len(list(group))} for key, group in itertools.groupby(sorted_data)],
-                               key=lambda x: x["count"])
-        return grouped_data
-
+        grouped_data = sorted(
+            [
+                {"age": key, "count": len(list(group))}
+                for key, group in itertools.groupby(sorted_data)
+            ],
+            key=lambda x: x["count"],
+        )
+        for child in grouped_data:
+            print(f"age: {child['age']}, count: {child['count']}")
