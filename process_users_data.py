301fc1b049557d8a265f9520c2f18033b29beca9
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-12-02 13:13:17 +0100

    add process users data

---
+++ process_users_data.py
@@ -0,0 +1,17 @@
+from users_data_handler import UsersDataMerger
+
+
+def process_users_data(files_paths):
+    users_data_merger = UsersDataMerger(files_paths)
+    return users_data_merger.get_merged_data_without_duplicates()
+
+
+files = [
+    "data/a/b/users_1.xml",
+    "data/users_2.xml",
+    "data/a/b/users_1.csv",
+    "data/a/c/users_2.csv",
+    "data/a/users.json"
+]
+
+data = process_users_data(files)
