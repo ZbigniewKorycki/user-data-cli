fc7fef916bc5786064bb242639d3d07decd63e94
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-12-02 13:12:55 +0100

    add cli actions output to print-oldest-account, group-by-age

--- cli.py
+++ cli.py
@@ -10,12 +10,21 @@ args: Namespace = parser.parse_args()
 
 if args.command == "print-all-accounts":
     action = Actions(login=args.login, password=args.password)
-    result = action.count_all_accounts()
+    result = action.count_all_users()
     print(result)
 elif args.command == "print-oldest-account":
-    pass
+    action = Actions(login=args.login, password=args.password)
+    oldest_account = action.get_oldest_account()
+    if oldest_account is not None:
+        print(
+            f"name: {oldest_account['firstname']}\n"
+            f"email_address: {oldest_account['email']}\n"
+            f"created_at: {oldest_account['created_at']}")
 elif args.command == "group-by-age":
-    pass
+    action = Actions(login=args.login, password=args.password)
+    result = action.get_users_children_grouped_by_age()
+    for child in result:
+        print(f"age: {child['age']}, count: {child['count']}")
 elif args.command == "print-children":
     pass
 elif args.command == "find-similar-children-by-age":
