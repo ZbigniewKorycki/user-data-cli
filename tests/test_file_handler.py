a0f6f7098f0c730619ea3f567a0b32645f206006
Zbigniew Korycki <zbigniewkorycki@gmail.com>
2023-12-03 15:07:18 +0100

    add test data in csv and json, add test for read json and csv file

--- tests/test_file_handler.py
+++ tests/test_file_handler.py
@@ -18,3 +18,33 @@ class TestFileHandler(unittest.TestCase):
         # Test case: File path without extension
         file_handler_no_extension = FileHandler("/path/to/example")
         self.assertIs(None, file_handler_no_extension.extract_file_extension())
+
+    def test_read_csv_file(self):
+        # Test case: Read csv test file
+        file_handler = FileHandler("./data/test_data.csv")
+        users_data = file_handler.read_csv_file()
+        self.assertIsInstance(users_data, list)
+        for user in users_data:
+            self.assertIsInstance(user, dict)
+            self.assertIn("firstname", user)
+            self.assertIn("telephone_number", user)
+            self.assertIn("email", user)
+            self.assertIn("password", user)
+            self.assertIn("role", user)
+            self.assertIn("created_at", user)
+            self.assertIn("children", user)
+
+    def test_read_json_file(self):
+        # Test case: Read json test file
+        file_handler = FileHandler("./data/test_data.json")
+        users_data = file_handler.read_json_file()
+        self.assertIsInstance(users_data, list)
+        for user in users_data:
+            self.assertIsInstance(user, dict)
+            self.assertIn("firstname", user)
+            self.assertIn("telephone_number", user)
+            self.assertIn("email", user)
+            self.assertIn("password", user)
+            self.assertIn("role", user)
+            self.assertIn("created_at", user)
+            self.assertIn("children", user)
